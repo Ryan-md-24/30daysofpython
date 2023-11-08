b7766d935e2f5a51abb89fb440c2a9e2d6f1e544
Ryan-md-24 <150283708+Ryan-md-24@users.noreply.github.com>
2023-11-09 00:23:04 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,4 @@
+x= int(input('Enter first value x'))
+y= int(input('Enter second value y'))
+value = pow(x,y)
+print('x power y is {value}')
---
+++ q4.py
@@ -0,0 +1,3 @@
+celsius= float(input("Enter your Celsius temp"))
+fahrenheit=(celsius*9/5)+32
+print ("Temperature in Fahrenheit is " +(fahrenheit))
